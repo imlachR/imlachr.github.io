f48e0d8452b720d1017db0b49cb836f4518c25ea
imlachR <rodrimlach@gmail.com>
2024-10-04 14:52:27 -0300

    WIP: Layout Change, Add Featured Work

---
+++ index.js
@@ -0,0 +1,224 @@
+(()=>{var n=`<div id="top" class="header-ctn">
+  
+  <nav class="nav">   
+    <figure class="profile-img-ctn-medium">
+      <img 
+        id="profile-image" 
+        class="profile-img"
+        loading="lazy"
+        src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/profile-image.jpg" 
+        alt="Profile Image">
+      <div class="online-ping">
+          <div class="blob"></div>
+      </div>
+    </figure>
+    <ul class="nav-right">
+      <li>
+        <a class="contact-link" href="https://github.com/imlachR">
+          <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.6868 0C4.77726 0 0 4.81248 0 10.7662C0 15.5253 3.06096 19.5538 7.30731 20.9796C7.83822 21.0868 8.03269 20.7479 8.03269 20.4629C8.03269 20.2133 8.01519 19.3578 8.01519 18.4664C5.04239 19.1082 4.42333 17.183 4.42333 17.183C3.94558 15.9352 3.23771 15.6145 3.23771 15.6145C2.26471 14.955 3.30858 14.955 3.30858 14.955C4.38789 15.0263 4.95423 16.0601 4.95423 16.0601C5.90951 17.6999 7.44884 17.2366 8.06812 16.9513C8.1565 16.2561 8.43978 15.7749 8.74056 15.5076C6.36954 15.258 3.87492 14.3311 3.87492 10.1957C3.87492 9.01924 4.29929 8.05675 4.97173 7.30819C4.86564 7.04088 4.49398 5.93554 5.07804 4.45614C5.07804 4.45614 5.98038 4.17089 8.01497 5.56126C8.88604 5.32559 9.78437 5.20571 10.6868 5.2047C11.5891 5.2047 12.5089 5.3296 13.3584 5.56126C15.3932 4.17089 16.2955 4.45614 16.2955 4.45614C16.8796 5.93554 16.5077 7.04088 16.4016 7.30819C17.0917 8.05675 17.4986 9.01924 17.4986 10.1957C17.4986 14.3311 15.004 15.24 12.6153 15.5076C13.0046 15.8462 13.3406 16.4878 13.3406 17.5039C13.3406 18.9476 13.3231 20.1063 13.3231 20.4627C13.3231 20.7479 13.5178 21.0868 14.0485 20.9798C18.2949 19.5535 21.3558 15.5253 21.3558 10.7662C21.3733 4.81248 16.5786 0 10.6868 0Z" fill="#0D112B"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a class="contact-link" href="mailto:rodrimlach@gmail.com?Subject=info%20services">
+          <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 0H0.8C0.587827 0 0.384344 0.0842856 0.234315 0.234315C0.0842854 0.384344 0 0.587827 0 0.8V14.4C0 14.8243 0.168571 15.2313 0.468629 15.5314C0.768687 15.8314 1.17565 16 1.6 16H19.2C19.6243 16 20.0313 15.8314 20.3314 15.5314C20.6314 15.2313 20.8 14.8243 20.8 14.4V0.8C20.8 0.587827 20.7157 0.384344 20.5657 0.234315C20.4157 0.0842856 20.2122 0 20 0ZM17.943 1.6L10.4 8.515L2.857 1.6H17.943ZM19.2 14.4H1.6V2.619L9.859 10.19C10.0066 10.3255 10.1996 10.4007 10.4 10.4007C10.6003 10.4007 10.7934 10.3255 10.941 10.19L19.2 2.619V14.4Z" fill="#0D112B"/>
+          </svg>
+        </a>
+      </li>
+    </ul> <!-- nav-right -->
+  </nav> <!-- nav -->
+  
+  <div class="header-intro">
+    <div class="header-intro-greetings">
+      <h1 class="text-big text-dark">Hi there, <br>I'm Rodrigo</h1>
+      <div class="header-ctv-ctn">
+        <a class="ctv" href="">Book a call</a>
+      </div>
+    </div> <!-- header-intro-greetings -->
+    <div class="header-intro-skills">
+      <h4 class="text-subsection">
+        A <span class="text-highlight">full-stack</span> developer, focusing on  
+        <span class="text-grey">
+          Elixir, Phoenix, LiveView, OTP, Postgres, Redis, SQL, Javascript, Docker, AWS, 
+          CI/CD, HTML, CSS, TailwindCSS, UX, and UI Design.
+        </span> 
+      </h4>
+      <h4 class="text-subsection">
+        I also have past experience with 
+        <span class="text-grey">Python, Ruby on Rails, React, Next.js, and Vue.js.</span> 
+      </h4>
+    </div> <!-- header-intro-skills -->
+  </div> <!-- header-intro -->
+
+</div> <!-- header-container -->`;var a=document.createElement("header");a.className="header";a.innerHTML=n;document.body.appendChild(a);var o=`<div class="container">
+  <h2 class="text-section-title">Featured Work</h2>
+</div>
+
+<div class="work-collection">
+
+  <div class="container">
+    <div class="work">
+      <div class="item-legend">
+        <h5 class="text-subsection">Architectural Design & Presentation</h5>
+        <p class="text-regular text-grey">
+          A modern interface blending architectural elegance with intuitive user flow, 
+          crafted to simplify complex workflows and enhance user engagement.
+        </p>
+      </div> <!-- item-legend -->
+      <div class="item-collection mb-12">
+        <figure class="image-container">
+          <div class="loading-overlay">
+            <div class="loading-spinner"></div>
+          </div> <!-- loading-overlay --> 
+          <img 
+            class="item-img" 
+            id="swiss-01" 
+            loading="lazy"
+            src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/swiss-01.svg" 
+            alt="Swiss Design 01">
+        </figure>
+        <figure class="image-container">
+          <div class="loading-overlay">
+            <div class="loading-spinner"></div>
+          </div> <!-- loading-overlay --> 
+          <img 
+            class="item-img" 
+            id="swiss-02" 
+            loading="lazy"
+            src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/swiss-02.svg" 
+            alt="Swiss Design 02">
+        </figure>
+      </div> <!-- item-collection -->
+    </div> <!-- work -->
+  </div>  <!-- container -->
+    
+  <div class="work-wide bg-light-grey">
+    <div class="container">
+      <div class="work">
+        <div class="item-legend">
+          <h5 class="text-subsection">Fashion as Unique <br>as You</h5>
+          <p class="text-regular text-grey">
+            An energetic, bold interface designed to inspire creativity and personal expression. 
+            The vibrant colors and dynamic layouts invite users to explore underground fashion, 
+            while seamless navigation enhances their shopping experience.
+          </p>
+        </div> <!-- item-legend -->
+        <div class="item-collection">
+          <figure class="image-container">
+            <div class="loading-overlay">
+              <div class="loading-spinner"></div>
+            </div> <!-- loading-overlay --> 
+            <img 
+              class="item-img" 
+              id="neo-01" 
+              loading="lazy"
+              src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/neo-b-01.svg" 
+              alt="Neo Brutalist Design 01">
+          </figure>
+          <figure class="image-container">
+            <div class="loading-overlay">
+              <div class="loading-spinner"></div>
+            </div> <!-- loading-overlay --> 
+            <img 
+              class="item-img" 
+              id="neo-02" 
+              loading="lazy"
+              src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/neo-b-02.svg" 
+              alt="Neo Brutalist Design 02">
+          </figure>
+        </div>
+      </div> <!-- work -->
+    </div> <!-- container -->
+  </div> <!-- work-wide -->
+
+  <div class="work-wide bg-light-grey">
+    <div class="container">
+      <div class="work">
+        <div class="item-legend">
+          <h5 class="text-subsection">Team Legacy Onboarding</h5>
+          <p class="text-regular text-grey">
+            An intuitive step-by-step guide for new users, designed to streamline the onboarding process. 
+            This checklist layout ensures clarity and focus, allowing new team members to navigate essential 
+            tasks with ease, fostering leadership and growth from the very start.
+          </p>
+        </div> <!-- item-legend -->
+        <div class="">
+          <img 
+            class="w-full single-web-img" 
+            id="legacy-web-01"
+            loading="lazy" 
+            src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/legacy-web-01.jpg" 
+            alt="Legacy Web Image">
+        </div>
+      </div> <!-- work -->
+    </div> <!-- container -->
+  </div> <!-- work-wide -->
+
+  <div class="container">
+    <div class="work mt-12">
+      <div class="item-legend">
+        <h5 class="text-subsection">Simplified and Mobile-Optimized</h5>
+        <p class="text-regular text-grey">
+          A streamlined, multi-step process that guides new users through key onboarding tasks, ensuring an effortless 
+          transition into the Legacy team. Mobile-friendly for leaders on the go.
+        </p>
+      </div> <!-- item-legend -->
+      <div class="item-collection">
+        <figure class="image-container">
+          <div class="loading-overlay">
+            <div class="loading-spinner"></div>
+          </div> <!-- loading-overlay --> 
+          <img 
+            class="item-img" 
+            id="legacy-01"
+            loading="lazy" 
+            src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/legacy-bef-01.svg" 
+            alt="Legacy Design 01">
+        </figure>
+        <figure class="image-container">
+          <div class="loading-overlay">
+            <div class="loading-spinner"></div>
+          </div> <!-- loading-overlay --> 
+          <img 
+            class="item-img" 
+            id="legacy-02"
+            loading="lazy" 
+            src="https://cf-simple-s3-origin-trivium-static-873738670572.s3.amazonaws.com/portfolio/legacy-bef-02.svg" 
+            alt="Legacy Design 02">
+        </figure>
+      </div> <!-- item-collection -->
+    </div> <!-- work -->
+  </div>  <!-- container -->
+
+</div>  <!-- work-collection -->`;var i=document.createElement("div");i.className="featured-work";i.innerHTML=o;document.body.appendChild(i);var g=i.querySelectorAll(".item-img");g.forEach(e=>{let t=e.previousElementSibling;e.complete?t.style.opacity="0":(e.addEventListener("load",()=>{t.style.opacity="0"}),e.addEventListener("error",()=>{t.innerHTML="Error loading image"}))});var s=`<div class="container">
+  <div class="footer-ctn">
+    <ul class="footer-links">
+      <li>
+        <a class="contact-link" href="https://github.com/imlachR">
+          <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.6868 0C4.77726 0 0 4.81248 0 10.7662C0 15.5253 3.06096 19.5538 7.30731 20.9796C7.83822 21.0868 8.03269 20.7479 8.03269 20.4629C8.03269 20.2133 8.01519 19.3578 8.01519 18.4664C5.04239 19.1082 4.42333 17.183 4.42333 17.183C3.94558 15.9352 3.23771 15.6145 3.23771 15.6145C2.26471 14.955 3.30858 14.955 3.30858 14.955C4.38789 15.0263 4.95423 16.0601 4.95423 16.0601C5.90951 17.6999 7.44884 17.2366 8.06812 16.9513C8.1565 16.2561 8.43978 15.7749 8.74056 15.5076C6.36954 15.258 3.87492 14.3311 3.87492 10.1957C3.87492 9.01924 4.29929 8.05675 4.97173 7.30819C4.86564 7.04088 4.49398 5.93554 5.07804 4.45614C5.07804 4.45614 5.98038 4.17089 8.01497 5.56126C8.88604 5.32559 9.78437 5.20571 10.6868 5.2047C11.5891 5.2047 12.5089 5.3296 13.3584 5.56126C15.3932 4.17089 16.2955 4.45614 16.2955 4.45614C16.8796 5.93554 16.5077 7.04088 16.4016 7.30819C17.0917 8.05675 17.4986 9.01924 17.4986 10.1957C17.4986 14.3311 15.004 15.24 12.6153 15.5076C13.0046 15.8462 13.3406 16.4878 13.3406 17.5039C13.3406 18.9476 13.3231 20.1063 13.3231 20.4627C13.3231 20.7479 13.5178 21.0868 14.0485 20.9798C18.2949 19.5535 21.3558 15.5253 21.3558 10.7662C21.3733 4.81248 16.5786 0 10.6868 0Z" fill="#0D112B"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a class="contact-link" href="mailto:rodrimlach@gmail.com?Subject=info%20services">
+          <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20 0H0.8C0.587827 0 0.384344 0.0842856 0.234315 0.234315C0.0842854 0.384344 0 0.587827 0 0.8V14.4C0 14.8243 0.168571 15.2313 0.468629 15.5314C0.768687 15.8314 1.17565 16 1.6 16H19.2C19.6243 16 20.0313 15.8314 20.3314 15.5314C20.6314 15.2313 20.8 14.8243 20.8 14.4V0.8C20.8 0.587827 20.7157 0.384344 20.5657 0.234315C20.4157 0.0842856 20.2122 0 20 0ZM17.943 1.6L10.4 8.515L2.857 1.6H17.943ZM19.2 14.4H1.6V2.619L9.859 10.19C10.0066 10.3255 10.1996 10.4007 10.4 10.4007C10.6003 10.4007 10.7934 10.3255 10.941 10.19L19.2 2.619V14.4Z" fill="#0D112B"/>
+          </svg>
+        </a>
+      </li>
+    </ul> <!-- footer-links -->
+    <a href="#top" class="back-to-top">
+      <span class="text-regular text-dark">Back to Top</span>
+      <figure class="contact-link">
+        <svg width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.75 7.75L7.25 1.25L13.75 7.75" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>          
+      </figure>
+    </a>
+  </div>
+</div>
+`;var r=document.createElement("footer");r.className="footer";r.innerHTML=s;document.body.appendChild(r);})();
+//# sourceMappingURL=index.js.map
